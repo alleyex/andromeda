5b83761106aa65353bf8967f5f4c4510722dcd60
lin jung chun <alleyex@gmail.com>
2019-07-17 23:45:11 +0800

    add app-routing.module.ts

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 ### deploy to github
 
 1. Run `npm install angular-cli-ghpages --save-dev` 
-2. Run `build --prod --aot --base-href "https://alleyex.github.io/andromeda/"` 
+2. Run `ng build --prod --aot --base-href "https://alleyex.github.io/andromeda/"` 
 3. Run `npx ngh --dir=dist/andromeda`
-4. website address [andromeda](https://alleyex.github.io/andromeda/)
+4. website  [andromeda](https://alleyex.github.io/andromeda/)
+
+### add app-routing.module
+
+Run `ng generate module app-routing --module app --flat`
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
